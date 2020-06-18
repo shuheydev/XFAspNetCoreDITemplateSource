a42a12ff0848f87499697c6fda5e9aa108ce5764
Shuhey <shuhey.dev@gmail.com>
2020-06-18 10:44:00 +0900

    Add project files.

---
+++ XFAspNetCoreDITemplate/XFAspNetCoreDITemplate/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XFAspNetCoreDITemplate
+{
+    public class Startup
+    {
+        public static IServiceProvider ServiceProvider { get; set; }
+        
+        public static void Init()
+        {
+
+        }
+
+        private void ConfigureServices(HostBuilderContext context,IServiceCollection services)
+        {
+
+        }
+    }
+}
