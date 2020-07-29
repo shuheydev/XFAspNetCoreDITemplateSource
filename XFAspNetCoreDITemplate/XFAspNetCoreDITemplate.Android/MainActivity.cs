623c3a125a29f84152f47302b7ed1f336c3aa3d7
shuheydev <shuhey.dev@gmail.com>
2020-07-29 23:21:36 +0900

    Refactor.

--- XFAspNetCoreDITemplate/XFAspNetCoreDITemplate.Android/MainActivity.cs
+++ XFAspNetCoreDITemplate/XFAspNetCoreDITemplate.Android/MainActivity.cs
@@ -26,7 +26,7 @@ namespace XFAspNetCoreDITemplate.Droid
 
             LoadApplication(Startup.Init(ConfigureServices));
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
